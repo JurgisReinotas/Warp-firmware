33f949861f4ce50c19be53f3b7189720333a0770
JurgisReinotas <73143884+JurgisReinotas@users.noreply.github.com>
2024-02-25 17:12:36 +0000

    Update devINA219.c

--- src/boot/ksdk1.1.0/devINA219.c
+++ src/boot/ksdk1.1.0/devINA219.c
@@ -1,1 +1,99 @@
-"Hello"
+#include <stdlib.h>
+
+/*
+ *	config.h needs to come first
+ */
+#include "config.h"
+
+#include "fsl_misc_utilities.h"
+#include "fsl_device_registers.h"
+#include "fsl_i2c_master_driver.h"
+#include "fsl_rtc_driver.h"
+#include "fsl_clock_manager.h"
+#include "fsl_power_manager.h"
+#include "fsl_mcglite_hal.h"
+#include "fsl_port_hal.h"
+
+#include "gpio_pins.h"
+#include "SEGGER_RTT.h"
+#include "warp.h"
+
+static int
+regReadINA219(uint8_t registerToRead, int numberOfBytes)
+{
+	i2c_status_t	status;
+  
+	i2c_device_t slave =
+	{
+		.address = 0x40,
+		.baudRate_kbps = gWarpI2cBaudRateKbps
+	};
+
+	status = I2C_DRV_MasterReceiveDataBlocking(
+		0 /* I2C peripheral instance */,
+		&slave,
+		NULL,
+		0,
+		registerToRead,
+		numberOfBytes,
+		gWarpI2cTimeoutMilliseconds);
+  
+  /*!
+    NULL for a command
+    0 for zero bytes
+    registerToRead - current register address
+    numberOfBytes for number of bytes of data
+  */
+
+	if (status != kStatus_I2C_Success)
+	{
+		return kWarpStatusDeviceCommunicationFailed;
+	}
+
+	return kWarpStatusOK;
+}
+
+static int
+regWriteINA219(uint8_t valueToWrite, int numberOfBytes)
+{
+	i2c_status_t	status;
+  
+	i2c_device_t slave =
+	{
+		.address = 0x40,
+		.baudRate_kbps = gWarpI2cBaudRateKbps
+	};
+
+	status = I2C_DRV_MasterSendDataBlocking(
+		0 /* I2C peripheral instance */,
+		&slave,
+		NULL,
+		0,
+		&valueToWrite,
+		numberOfBytes,
+		gWarpI2cTimeoutMilliseconds);
+  
+  /*!
+    NULL for a command
+    0 for zero bytes
+    &valueToWrite - pointer to the register of the value to write
+    numberOfBytes - number of bytes of data
+  */
+
+	if (status != kStatus_I2C_Success)
+	{
+		return kWarpStatusDeviceCommunicationFailed;
+	}
+
+	return kWarpStatusOK;
+}
+
+
+int
+devINA219init()
+{
+  warpEnableI2Cpins();
+
+  
+	return;
+}
