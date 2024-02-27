85531f12ec33bff3e6a1a456dec0d69d0da725a6
JurgisReinotas <73143884+JurgisReinotas@users.noreply.github.com>
2024-02-27 17:57:20 +0000

    Update devINA219.c

--- src/boot/ksdk1.1.0/devINA219.c
+++ src/boot/ksdk1.1.0/devINA219.c
@@ -38,7 +38,7 @@ WarpStatus
 writeSensorRegisterINA219(uint8_t deviceRegister, uint16_t payload)
 {
 	uint8_t		commandByte[1];
-	uint8_t		payloadByte[1];
+	uint8_t		payloadByte[2];
 	i2c_status_t	status;
 
 	switch (deviceRegister)
