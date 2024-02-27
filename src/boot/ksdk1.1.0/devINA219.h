34e038fadc5f3b11a4e31037102f91038e9adcec
JurgisReinotas <73143884+JurgisReinotas@users.noreply.github.com>
2024-02-27 11:09:52 +0000

    Update devINA219.h

--- src/boot/ksdk1.1.0/devINA219.h
+++ src/boot/ksdk1.1.0/devINA219.h
@@ -1,10 +1,10 @@
 
-void		    initINA219(const uint8_t i2cAddress, uint16_t operatingVoltageMillivolts);
+void		initINA219(const uint8_t i2cAddress, uint16_t operatingVoltageMillivolts);
 WarpStatus	readSensorRegisterINA219(uint8_t deviceRegister, int numberOfBytes);
 WarpStatus	writeSensorRegisterINA219(uint8_t deviceRegister, uint8_t payloadBtye);
 WarpStatus 	configureSensorINA219(uint8_t payloadF_SETUP, uint8_t payloadCTRL_REG1);
-void		    printSensorDataINA219(bool hexModeFlag);
-uint8_t		  appendSensorDataINA219(uint8_t* buf);
+void		printSensorDataINA219(bool hexModeFlag);
+uint8_t		appendSensorDataINA219(uint8_t* buf);
 
 const uint8_t bytesPerMeasurementINA219            = 6;
 const uint8_t bytesPerReadingINA219                = 2;
