2241bcbf169e96937ec3556d9f6c9888fab86539
JurgisReinotas <73143884+JurgisReinotas@users.noreply.github.com>
2024-02-28 12:44:52 +0000

    Update boot.c

--- src/boot/ksdk1.1.0/boot.c
+++ src/boot/ksdk1.1.0/boot.c
@@ -3228,8 +3228,8 @@ writeAllSensorsToFlash(int menuDelayBetweenEachRun, int loopForever)
 
 #if (WARP_BUILD_ENABLE_DEVINA219)
 	numberOfConfigErrors += configureSensorINA219(
-		0x199F, /* Payload: Disable FIFO */
-		0x0800  /* Normal read 8bit, 800Hz, normal, active mode */
+		0x399F, /* Payload: Disable FIFO */
+		0x1000  /* Normal read 8bit, 800Hz, normal, active mode */
 	);
 	sensorBitField = sensorBitField | kWarpFlashINA219BitField;
 #endif
@@ -3493,8 +3493,8 @@ printAllSensors(bool printHeadersAndCalibration, bool hexModeFlag,
 
 #if (WARP_BUILD_ENABLE_DEVINA219)
 	numberOfConfigErrors += configureSensorINA219(
-		0x199F, /* Payload: Disable FIFO */
-		0x0800  /* Normal read 8bit, 800Hz, normal, active mode */
+		0x399F, /* Payload: Disable FIFO */
+		0x1000  /* Normal read 8bit, 800Hz, normal, active mode */
 	);
 #endif
 
