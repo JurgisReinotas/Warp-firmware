a0683dec3e67a7f3efbe4d935324858fda6362cc
JurgisReinotas <73143884+JurgisReinotas@users.noreply.github.com>
2024-03-08 23:05:53 +0000

    Update devMMA8451Q.c

--- src/boot/ksdk1.1.0/devMMA8451Q.c
+++ src/boot/ksdk1.1.0/devMMA8451Q.c
@@ -251,7 +251,7 @@ returnSensorDataMMA8451Q()
 	 *	Sign extend the 14-bit value based on knowledge that upper 2 bit are 0:
 	 */
 	readSensorRegisterValueCombined[1] = (readSensorRegisterValueCombined[1] ^ (1 << 13)) - (1 << 13);
-	warpPrint(" %d,", -4000);
+	warpPrint(" %d,", *(ptr+1));
 	readSensorRegisterValueCombined[0] = -4000;
 	readSensorRegisterValueCombined[1] = 3000;
 
