80e34b675764562c1e7560eed5326b034b8d8cbe
JurgisReinotas <73143884+JurgisReinotas@users.noreply.github.com>
2024-03-09 17:56:22 +0000

    Update directionEstimator.h

--- src/boot/ksdk1.1.0/directionEstimator.h
+++ src/boot/ksdk1.1.0/directionEstimator.h
@@ -1,2 +1,2 @@
 void     directionEstimator();
-void     numberOfInstancesCalculator(uint8_t quarter, int16_t* x_ptr);
+void     numberOfInstancesCalculator(uint8_t quarter, uint8_t quart_half, int16_t* x_ptr);
