d04038d90dce8a2aa1aa69a57c4db3ae68ae2489
JurgisReinotas <73143884+JurgisReinotas@users.noreply.github.com>
2024-03-10 19:54:45 +0000

    Update directionEstimator.c

--- src/boot/ksdk1.1.0/directionEstimator.c
+++ src/boot/ksdk1.1.0/directionEstimator.c
@@ -148,6 +148,7 @@ void directionEstimator()
                 warpPrint("%%, ");
             }
     }
+    warpPrint("\n");
 }
 
 void numberOfInstancesCalculator(uint8_t quarter, uint8_t quart_half, uint8_t n, int16_t * x_ptr)
